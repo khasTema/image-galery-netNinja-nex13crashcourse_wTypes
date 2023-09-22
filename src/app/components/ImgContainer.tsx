3510c33753c0ac1a3ab787c9617a33745255f3e4
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-09-22 14:48:51 +0200

    galery and base64 blur

---
+++ src/app/components/ImgContainer.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import { Photo } from "@/models/Images"
+
+type Props = {
+    photo: Photo
+}
+
+export default function ImgContainer({photo}: Props) {
+  return (
+    <div key={photo.id} className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group">
+        <Image  
+            src={photo.src.large}
+            alt={photo.alt}
+            fill={true}
+            className="object-cover group-hover:opacity-75"
+            sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+            placeholder="blur"
+            blurDataURL={photo.blurredDataUrl}
+        />
+        
+    </div>
+  )
+}
